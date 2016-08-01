0e6994fff494580d954d1d4f02fcaeb9bd6761e5
José Luís Sáez Sánchez <joseluissaez@me.com>
2016-08-01 20:21:39 +0200

    Acabamos con los test de Wallet, vuelvo a guardar por fallo en el xcode

--- FusWalletTests/FUSWalletTests.m
+++ FusWalletTests/FUSWalletTests.m
@@ -7,6 +7,7 @@
 //
 
 #import <XCTest/XCTest.h>
+
 #import "FUSMoney.h"
 #import "FUSBroker.h"
 #import "FUSWallet.h"
@@ -17,23 +18,6 @@
 
 @implementation FUSWalletTests
 
-- (void)setUp {
-    [super setUp];
-    
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-
-    // In UI tests it is usually best to stop immediately when a failure occurs.
-    self.continueAfterFailure = NO;
-    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-    [[[XCUIApplication alloc] init] launch];
-
-    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
 
 // €40 + $20 = $100 2:1
 
@@ -41,7 +25,7 @@
 -(void) testAdditionWithReduction{
     
     FUSBroker *broker = [FUSBroker new];
-    [broker addRate: 2 fromCurrency:@"USD" toCurrency:@"EUR"];
+    [broker addRate: 2 fromCurrency:@"EUR" toCurrency:@"USD"];
     
     FUSWallet *wallet = [[FUSWallet alloc] initWithAmount:40 currency:@"EUR"];
     [wallet plus: [FUSMoney dollarWithAmount:20]];
